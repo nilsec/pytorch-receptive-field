a732fc2d954259a3ea10e52b573c8c5150419cd5
nilsec <nils.eckstein@googlemail.com>
2021-03-02 16:19:47 -0500

    Add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='torch_receptive_field',
+    version='0.1',
+    description='Neurotransmitter classification',
+    url='https://github.com/funkelab/synister',
+    author='Funkelab',
+    packages=[
+        'torch_receptive_field',
+        ])
